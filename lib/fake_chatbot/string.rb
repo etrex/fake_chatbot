abdae85bbe8e1902e4d298c8e0f1901d1f37e45c
etrex kuo <et284vu065k3@gmail.com>
2019-01-09 10:48:50 +0800

    add doc

--- lib/fake_chatbot/string.rb
+++ lib/fake_chatbot/string.rb
@@ -1,5 +1,19 @@
 # the core of chinese chatbot
 class String
+  # an simple reply for chatbot demo, like echo.
+  # Example:
+  #   >> '在嗎?'.fake_chatbot
+  #   => 在!
+  #
+  #  >> '你好'.fake_chatbot
+  #  => 你好
+  #
+  #  >> '能聽懂中文嗎??'.fake_chatbot
+  #  => 能聽懂中文!!
+  #
+  #  >> '真的嗎?'.fake_chatbot
+  #  => 真的!
+
   def fake_chatbot
     tr('嗎', '').tr('?？', '!！')
   end
